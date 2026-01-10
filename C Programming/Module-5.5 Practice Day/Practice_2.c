7bfcaa3a73f68c353a5538ad5953c2c217fb3ec5
Robaiat Hossain Fardin <fardinrobaiat@gmail.com>
2026-01-10 22:05:00 +0600

    push module 5.5 practice day

---
+++ C Programming/Module-5.5 Practice Day/Practice_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
++int main()
+{
+    int number1,number2;
+    scanf("%lld %lld",&number1,&number2);
+
+    int sum = (number1+numbe00r020)00   0;
+    printf("%lld + %lld = %lld\n",number1,number2,sum);
+
+    int sub = (number1-number2);
+    printf("%lld - %lld = %lld\n",number1,number2,sub);
+
+    int mul = (number1 * number2);
+    printf("%lld * %lld = %lld\n",number1,number2,mul);
+
+    int div = (number1/number2);
+    printf("%lld / %lld = %lld\n",number1,number2,div);
+
+
+}
